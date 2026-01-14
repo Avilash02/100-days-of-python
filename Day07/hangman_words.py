fe1a66f51545a31a7f557799d9535fdb652fd94a
Avilash Achar <avilashachar100@gmail.com>
2026-01-14 22:25:55 +0530

    Day 07: Hangman game

---
+++ Day07/hangman_words.py
@@ -0,0 +1,17 @@
+word_list = [
+    "cat",
+    "dog",
+    "sun",
+    "hat",
+    "pen",
+    "book",
+    "tree",
+    "ball",
+    "milk",
+    "fish",
+    "apple",
+    "bread",
+    "chair",
+    "table",
+    "phone"
+]
